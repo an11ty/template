4311de20a2c1328ba36a8fd727d13c29d4a22dbe
Tobias Davis <accounts@davistobias.com>
2020-08-26 00:33:15 -0500

    webpack stuff tidied up although i do not understand it quite yet

--- _src/scripts/scripts.11ty.js
+++ _src/scripts/scripts.11ty.js
@@ -8,85 +8,79 @@ const mfs = new MemoryFileSystem()
 
 // main entry point name
 const ENTRY_FILE_NAME = 'main.js'
+const OUTPUT_FILE_NAME = 'main.js'
+const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`)
+const outputPath = `_src/scripts/${OUTPUT_FILE_NAME}`
+const mapOutputPath = path.resolve(__dirname, '../../_site/_src/scripts/')
+
+// Compile JS with Webpack, write the result to Memory Filesystem.
+// this brilliant idea is taken from Mike Riethmuller / Supermaya
+// @see https://github.com/MadeByMike/supermaya/blob/master/site/utils/compile-webpack.js
+const compile = async webpackConfig => {
+	const compiler = webpack(webpackConfig)
+	compiler.outputFileSystem = mfs
+	compiler.inputFileSystem = fs
+	compiler.resolvers.normal.fileSystem = mfs
+
+	return new Promise((resolve, reject) => {
+		compiler.run((err, stats) => {
+			if (err || stats.hasErrors()) {
+				return reject(err || (stats.compilation ? stats.compilation.errors : null))
+			}
+
+			const { assets } = stats.compilation
+			const file = assets[ENTRY_FILE_NAME].source()
+			resolve(file)
+		})
+	})
+}
+
+// Main Config
+const defaultWebpackConfig = {
+	mode: isProd ? 'production' : 'development',
+	entry: entryPath,
+	output: {
+		path: path.resolve(__dirname, '../../memory-fs/js/')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [ '@babel/preset-env' ],
+						plugins: [ '@babel/plugin-transform-runtime' ]
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.EnvironmentPlugin({
+			ELEVENTY_ENV: process.env.ELEVENTY_ENV
+		})
+	]
+}
 
 module.exports = class {
-    // Configure Webpack in Here
-    async data() {
-        const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`)
-        const outputPath = path.resolve(__dirname, '../../memory-fs/js/')
+	// Configure Webpack in Here
+	async data() {
+		return {
+			permalink: `/_src/scripts/${ENTRY_FILE_NAME}`,
+			eleventyExcludeFromCollections: true,
+			webpackConfig: defaultWebpackConfig
+		}
+	}
 
-        // Transform .js files, run through Babel
-        const rules = [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-runtime']
-                    }
-                }
-            }
-        ]
-
-        // pass environment down to scripts
-        const envPlugin = new webpack.EnvironmentPlugin({
-            ELEVENTY_ENV: process.env.ELEVENTY_ENV
-        })
-
-        // Main Config
-        const webpackConfig = {
-            mode: isProd ? 'production' : 'development',
-            entry: entryPath,
-            output: { path: outputPath },
-            module: { rules },
-            plugins: [envPlugin]
-        }
-
-        return {
-            permalink: `/assets/scripts/${ENTRY_FILE_NAME}`,
-            eleventyExcludeFromCollections: true,
-            webpackConfig
-        }
-    }
-
-    // Compile JS with Webpack, write the result to Memory Filesystem.
-    // this brilliant idea is taken from Mike Riethmuller / Supermaya
-    // @see https://github.com/MadeByMike/supermaya/blob/master/site/utils/compile-webpack.js
-    compile(webpackConfig) {
-        const compiler = webpack(webpackConfig)
-        compiler.outputFileSystem = mfs
-        compiler.inputFileSystem = fs
-        compiler.resolvers.normal.fileSystem = mfs
-
-        return new Promise((resolve, reject) => {
-            compiler.run((err, stats) => {
-                if (err || stats.hasErrors()) {
-                    const errors =
-                        err ||
-                        (stats.compilation ? stats.compilation.errors : null)
-
-                    reject(errors)
-                    return
-                }
-
-                const { assets } = stats.compilation
-                const file = assets[ENTRY_FILE_NAME].source()
-
-                resolve(file)
-            })
-        })
-    }
-
-    // render the JS file
-    async render({ webpackConfig }) {
-        try {
-            const result = await this.compile(webpackConfig)
-            return result
-        } catch (err) {
-            console.log(err)
-            return null
-        }
-    }
+	// render the JS file
+	async render({ webpackConfig }) {
+		try {
+			return await compile(webpackConfig)
+		} catch (err) {
+			console.log(err)
+			return null
+		}
+	}
 }
