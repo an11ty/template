a26c1dbb9ae3869b7b21f44424f01817de222eb2
Tobias Davis <accounts@davistobias.com>
2020-08-25 23:55:49 -0500

    woot css with proper sourcemapping is working

--- _src/styles/styles.11ty.js
+++ _src/styles/styles.11ty.js
@@ -8,111 +8,110 @@ const isProd = process.env.ELEVENTY_ENV === 'production'
 
 // main entry point name
 const ENTRY_FILE_NAME = 'main.scss'
+const OUTPUT_FILE_NAME = 'main.css'
+const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`)
+const outputPath = `_src/styles/${OUTPUT_FILE_NAME}`
+const mapOutputPath = path.resolve(__dirname, '../../_site/_src/styles/')
+
+// display an error overlay when CSS build fails.
+// this brilliant idea is taken from Mike Riethmuller / Supermaya
+// @see https://github.com/MadeByMike/supermaya/blob/master/site/utils/compile-scss.js
+const renderError = error => `
+	/* Error compiling stylesheet */
+	*,
+	*::before,
+	*::after {
+		box-sizing: border-box;
+	}
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+		min-height: 100vh;
+		font-family: monospace;
+		font-size: 1.25rem;
+		line-height:1.5;
+	} 
+	body::before { 
+		content: ''; 
+		background: #000;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+		height: 100%;
+		opacity: 0.7;
+		position: fixed;
+	}
+	body::after { 
+		content: '${cssesc(error)}'; 
+		white-space: pre;
+		display: block;
+		top: 0; 
+		padding: 30px;
+		margin: 50px;
+		width: calc(100% - 100px);
+		color: #721c24;
+		background-color: #f8d7da;
+		border: solid 2px red;
+		position: fixed;
+	}`
+
+// Compile Sass to CSS,
+// Embed Source Map in Development
+const compile = async config => new Promise((resolve, reject) => {
+	config.sourceMap = true
+	config.sourceMapEmbed = true
+	config.sourceComments = true
+	config.outputStyle = 'expanded'
+	return sass.render(config, (err, result) => {
+		if (err) {
+			return reject(err)
+		}
+		resolve(result.css.toString())
+	})
+})
+
+// Minify & Optimize with CleanCSS in Production
+const minify = async css => new Promise((resolve, reject) => {
+	const minified = new CleanCSS({
+		sourceMap: true,
+		sourceMapInlineSources: true
+	}).minify(css)
+
+	fs.writeFileSync(path.join(mapOutputPath, `${OUTPUT_FILE_NAME}.map`), minified.sourceMap.toString(), 'utf8')
+
+	return minified.error
+		? reject(minified.error)
+		: resolve(minified.styles + '\n' + `/*# sourceMappingURL=/${outputPath}.map */`)
+})
 
 module.exports = class {
-    async data() {
-        const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`)
-        return {
-            permalink: `/assets/styles/main.css`,
-            eleventyExcludeFromCollections: true,
-            entryPath
-        }
-    }
-
-    // Compile Sass to CSS,
-    // Embed Source Map in Development
-    async compile(config) {
-        return new Promise((resolve, reject) => {
-            if (!isProd) {
-                config.sourceMap = true
-                config.sourceMapEmbed = true
-                config.outputStyle = 'expanded'
-            }
-            return sass.render(config, (err, result) => {
-                if (err) {
-                    return reject(err)
-                }
-                resolve(result.css.toString())
-            })
-        })
-    }
-
-    // Minify & Optimize with CleanCSS in Production
-    async minify(css) {
-        return new Promise((resolve, reject) => {
-            if (!isProd) {
-                resolve(css)
-            }
-            const minified = new CleanCSS().minify(css)
-            if (!minified.styles) {
-                return reject(minified.error)
-            }
-            resolve(minified.styles)
-        })
-    }
-
-    // display an error overlay when CSS build fails.
-    // this brilliant idea is taken from Mike Riethmuller / Supermaya
-    // @see https://github.com/MadeByMike/supermaya/blob/master/site/utils/compile-scss.js
-    renderError(error) {
-        return `
-        /* Error compiling stylesheet */
-        *,
-        *::before,
-        *::after {
-            box-sizing: border-box;
-        }
-        html,
-        body {
-            margin: 0;
-            padding: 0;
-            min-height: 100vh;
-            font-family: monospace;
-            font-size: 1.25rem;
-            line-height:1.5;
-        } 
-        body::before { 
-            content: ''; 
-            background: #000;
-            top: 0;
-            bottom: 0;
-            width: 100%;
-            height: 100%;
-            opacity: 0.7;
-            position: fixed;
-        }
-        body::after { 
-            content: '${cssesc(error)}'; 
-            white-space: pre;
-            display: block;
-            top: 0; 
-            padding: 30px;
-            margin: 50px;
-            width: calc(100% - 100px);
-            color:#721c24;
-            background: #f8d7da;
-            border: solid 2px red;
-            position: fixed;
-        }`
-    }
-
-    // render the CSS file
-    async render({ entryPath }) {
-        try {
-            const css = await this.compile({ file: entryPath })
-            const result = await this.minify(css)
-            return result
-        } catch (err) {
-            // if things go wrong
-            if (isProd) {
-                // throw and abort in production
-                throw new Error(err)
-            } else {
-                // otherwise display the error overly
-                console.error(err)
-                const msg = err.formatted || err.message
-                return this.renderError(msg)
-            }
-        }
-    }
+	async data() {
+		return {
+			permalink: outputPath,
+			eleventyExcludeFromCollections: true,
+			entryPath
+		}
+	}
+	async render({ entryPath }) {
+		try {
+			const css = await compile({
+				file: entryPath
+			})
+			if (!isProd) {
+				return css
+			}
+			return await minify(css)
+		} catch (err) {
+			// if things go wrong in production we want
+			// to fully halt, but otherwise we can display
+			// an error overlay for easier development
+			if (isProd) {
+				throw new Error(err)
+			} else {
+				console.error(err)
+				return renderError(err.formatted || err.message)
+			}
+		}
+	}
 }
