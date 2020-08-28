28732bf3ac6dc3099a6dbe6ab583f99f8b9699df
Tobias Davis <accounts@davistobias.com>
2020-08-28 00:18:12 -0500

    metadata as js instead of json for comments mostly

---
+++ _data/metadata.js
@@ -0,0 +1,34 @@
+module.exports = {
+	title: 'Your Blog Name',
+	lang: 'en',
+	locale: 'en_us',
+	url: 'https://example.com/',
+	description: 'I am writing about my experiences as a naval navel-gazer.',
+	author: {
+		name: 'Your Name Here',
+		email: 'youremailaddress@example.com',
+		url: 'https://example.com/about-me/'
+	},
+
+	feed: {
+		subtitle: 'I am writing about my experiences as a naval navel-gazer.',
+		filename: 'feed.xml',
+		path: '/feed/feed.xml',
+		id: 'https://example.com/'
+	},
+	jsonfeed: {
+		path: '/feed/feed.json',
+		url: 'https://example.com/feed/feed.json'
+	},
+
+	// All the SVG icons in `_src/icons/` are available to be
+	// bundled up and used using `iconsprite`, but you don't
+	// want to add them to your HTML if they aren't used, so
+	// here you can list the icons that you want to embed and
+	// only those will be included. Leave the array empty or
+	// remove `icons` to not embed any, or set `icons` to `true`
+	// to embed all in the folder.
+	embeddedIcons: [
+		'github.svg'
+	]
+}
